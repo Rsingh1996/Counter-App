ac13bb3a72641547b6482a5a92f6f03ef0dd3340
Rahul <rahulna1996@gmail.com>
2021-05-30 14:07:50 +0530

    refector the code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Counter App
 
-App Link :: "http://rsingh1996.github.io/counter-app"
+App Link : https://rsingh1996.github.io/Counter-App/
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,28 +3,33 @@ import React, { Fragment, useState } from "react";
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
-  const increaseCounter = () => {
-    setCounter(counter + 1);
-  };
-  const decreaseCounter = () => {
-    if (counter > 0) setCounter(counter - 1);
-  };
-  const resetCounter = () => {
-    setCounter(0);
-  };
-
   return (
     <Fragment>
       <div id="counter-value">
         <h1>{counter}</h1>
       </div>
-      <button id="decrement-btn" onClick={decreaseCounter}>
+      <button
+        id="decrement-btn"
+        onClick={() => {
+          if (counter > 0) setCounter(counter - 1);
+        }}
+      >
         -
       </button>
-      <button id="reset-btn" onClick={resetCounter}>
+      <button
+        id="reset-btn"
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
         0
       </button>
-      <button id="increment-btn" onClick={increaseCounter}>
+      <button
+        id="increment-btn"
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
         +
       </button>
     </Fragment>
